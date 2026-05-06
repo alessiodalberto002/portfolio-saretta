aa6da8262547112335658081b527d0037b0038a8
alessiodalberto002 <aledalberto002.com>
2026-05-06 21:56:13 +0200

    proposta hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,41 +1,37 @@
-import imgHero from "../../assets/img/Screenshot 2026-03-15 alle 15.01.59.png"
+
+// Importa le tue foto (ne servono almeno 4-5 per l'effetto loop)
+import copScalo from "../../assets/img/scalo/14_luglio_copertina.jpg"
+import copAmami from "../../assets/img/amami/02.png"
+import imgScalo3 from "../../assets/img/scalo/03_ottobre.png"
+import imgAmami1 from "../../assets/img/amami/01.png"
+import imgAmami2 from "../../assets/img/amami/02.png"
+import img7p1 from "../../assets/img/7p/01.png"
+import img7p2 from "../../assets/img/7p/02 (1).png"
 
 const Hero = () => {
+    // Array originale delle immagini
+    const originalPhotos = [copScalo, imgScalo3, copAmami, imgAmami1, imgAmami2, img7p1, img7p2];
+
+    // Duplichiamo l'array una volta sola per permettere il loop fluido
+    const photos = [...originalPhotos, ...originalPhotos, ...originalPhotos, ...originalPhotos, ...originalPhotos, ...originalPhotos];
+
     return (
-        <section id="Home" className="vh-100 w-100 d-flex justify-content-center align-items-center overflow-hidden position-relative">
+        <section id="Home" className="hero-minimal w-100 mt-4">
 
-            {/* Container principale */}
-            <div className="position-relative d-flex flex-column justify-content-center align-items-center h-100 w-100">
-
-                {/* Immagine: h-75 su desktop, un po' più piccola su mobile per far spazio al testo */}
-                <div className="col-10 col-lg-auto h-75 h-lg-75">
-                    <img
-                        className="w-100 h-100 object-fit-cover"
-                        src={imgHero}
-                        alt="Hero"
-                    />
+            <div className="h-100 w-100 d-flex flex-column justify-content-center align-items-center">
+                <div className="hero-slider-container">
+                    <div className="hero-slider-track">
+                        {photos.map((src, index) => (
+                            <div key={index} className="hero-slide">
+                                <img src={src} alt={`Work ${index}`} className="img-fluid" />
+                            </div>
+                        ))}
+                    </div>
                 </div>
 
-                {/* "Think" */}
-                <h1 className="text-uppercase Aktiv-Grotesk-Thin hero-text position-absolute"
-                    style={{
-                        zIndex: 2,
-                        // Desktop logic: le tue coordinate originali
-                        // Mobile logic: centrato orizzontalmente e posizionato in alto
-                    }}>
-                    Think
-                </h1>
-
-                {/* "Create" */}
-                <h1 className="text-uppercase Aktiv-Grotesk-Thin hero-text position-absolute"
-                    style={{
-                        zIndex: 2,
-                        // Desktop logic: le tue coordinate originali
-                        // Mobile logic: centrato orizzontalmente e posizionato subito sotto Think
-                    }}>
-                    Create
-                </h1>
-
+                <div className="hero-footer w-100 d-flex justify-content-end pe-4 pe-lg-5">
+                    <h1 className="hero-title fw-lighter text-uppercase m-0">(Think Create)</h1>
+                </div>
             </div>
 
         </section>
