3dce699e411105f63dcda23dcb1028169ee195d8
alessiodalberto002 <aledalberto002.com>
2026-07-30 21:03:12 +0200

    modifica pb testo hero pt.4

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -82,7 +82,7 @@ const Hero = () => {
                 <motion.div initial={{ scale: 0.8, opacity: 0 }}
                         animate={{ scale: 1, opacity: 1 }}
                         transition={{ duration: 1, ease: "backOut" }}
-                        className='flex Aktiv-Grotesk-Thin text-center mb-20 p-4'>
+                        className='flex Aktiv-Grotesk-Thin text-center pb-20 px-4'>
 
                     <p className="text-xl md:text-2xl">*faccio cose belle per evitare di dovermi trovare un lavoro noioso</p>
                 </motion.div>
